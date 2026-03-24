ad581720fc75e6905e238503fa022b6f6c10a427
Elvis Njaramba <kigutanjaramba@gmail.com>
2026-03-24 14:48:00 +0300

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -103,7 +103,7 @@ export default function Login() {
               </div>
             </div>
 
-            {/* Remember me & Forgot password */}
+            /* {/* /* {/* Remember me & Forgot password */}
             <div className="flex items-center justify-between">
               <label className="flex items-center">
                 <input type="checkbox" className="rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
@@ -112,7 +112,7 @@ export default function Login() {
               <a href="#" className="text-sm text-blue-600 hover:text-blue-800 transition-colors">
                 Forgot password?
               </a>
-            </div>
+            </div> */ */} */
 
             {/* Login Button */}
             <button
