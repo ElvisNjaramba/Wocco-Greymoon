019ee4ba56088c05ee05eef72a902f8f9eba8fe7
ElvisNjaramba <kigutanjaramba11@gmail.com>
2026-03-26 17:56:28 +0300

    Updated Commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://38.247.148.50:10001/api/",
 });
 
 // Attach JWT to every request
